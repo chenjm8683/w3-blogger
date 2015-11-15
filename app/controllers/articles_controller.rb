1a35624aaf920700f405a976f201aab695f35826
Jingming Chen <chenjm8683@gmail.com>
2015-11-15 13:12:39 -0800

    Article#new, delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,37 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new
+    # @article.title = params[:article][:title]
+    # @article.body = params[:article][:body]
+
+    # @article = Article.new(
+    #   title: params[:article][:title],
+    #   body: params[:article][:body],
+    # )
+
+    # @article = Article.new(params[:article])
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to action: "index"
   end
 end
